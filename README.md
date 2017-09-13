75152b95e923e7807378b672e2e2f32c7a4774ba
daniellzl <mr.daniel.liang@gmail.com>
2017-09-13 15:59:41 -0700

    fixed live application link in Readme; changed indent size

--- README.md
+++ README.md
@@ -1,11 +1,27 @@
 # Camper Leaderboard
 
-This website displays a list of top 100 FreeCodeCampers ordered by points earned in past 30 days or from all time.
+Displays a list of top 100 FreeCodeCampers ordered by points earned in past 30 days or from all time.
 
-## Website
+[Live Application](camper-leaderboard-dlzl.surge.sh)
 
-http://camper-leaderboard-dlzl.surge.sh
+### Project Goals
 
-## Credit:
+Application was completed as a freeCodeCamp [challenge](https://www.freecodecamp.org/challenges/build-a-camper-leaderboard).
 
-https://www.freecodecamp.org/challenges/build-a-camper-leaderboard
+1. User Story: I can see a table of the freeCodeCamp campers who've earned the most brownie points in the past 30 days.
+
+2. User Story: I can see how many brownie points they've earned in the past 30 days, and how many they've earned total.
+
+3. User Story: I can toggle between sorting the list by how many brownie points they've earned in the past 30 days and by how many brownie points they've earned total.
+
+4. Hint: To get the top 100 campers for the last 30 days: https://fcctop100.herokuapp.com/api/fccusers/top/recent.
+
+5. Hint: To get the top 100 campers of all time: https://fcctop100.herokuapp.com/api/fccusers/top/alltime.
+
+### Technologies
+
+* babel
+* bootstrap
+* google fonts
+* jquery
+* react
--- scripts.js
+++ scripts.js
@@ -1,81 +1,76 @@
 /*
-██   ██ ███████  █████  ██████  ███████ ██████
-██   ██ ██      ██   ██ ██   ██ ██      ██   ██
-███████ █████   ███████ ██   ██ █████   ██████
-██   ██ ██      ██   ██ ██   ██ ██      ██   ██
-██   ██ ███████ ██   ██ ██████  ███████ ██   ██
+██   ██ ███████  █████  ██████  ███████
+██   ██ ██      ██   ██ ██   ██ ██
+███████ █████   ███████ ██   ██ █████
+██   ██ ██      ██   ██ ██   ██ ██
+██   ██ ███████ ██   ██ ██████  ███████
 */
 
+// title header for page
 const Header = () => {
-	// title header for page
-
-    return (
-        <div className="header">
-            <a href="https://www.freecodecamp.com">
-                <img className="FCCLogo" src="https://www.freecodecamp.com/design-style-guide/img/freeCodeCamp.svg" alt="FreeCodeCamp"/>
-            </a>
-        </div>
-    );
+  return (
+    <div className="header">
+      <a href="https://www.freecodecamp.com">
+        <img className="FCCLogo" src="https://www.freecodecamp.com/design-style-guide/img/freeCodeCamp.svg" alt="FreeCodeCamp"/>
+      </a>
+    </div>
+  );
 }
 
 /*
-████████  █████  ██████  ██      ███████
-   ██    ██   ██ ██   ██ ██      ██
-   ██    ███████ ██████  ██      █████
-   ██    ██   ██ ██   ██ ██      ██
-   ██    ██   ██ ██████  ███████ ███████
+██      ███████  █████  ██████  ███████
+██      ██      ██   ██ ██   ██ ██
+██      █████   ███████ ██   ██ █████
+██      ██      ██   ██ ██   ██ ██
+███████ ███████ ██   ██ ██████  ███████
 */
 
+// leader table displaying campers ranked by points
 const LeaderTable = (props) => {
-	// leader table displaying campers ranked by points
 
-	// for each camper, display row of camper statistics
-    const leaderTable = props.leaders.map((leader, index) => {
-        return (
-            <tr key={leader.username}>
-                <td>{index + 1}</td>
-                <td>
-                    <a href={"https://www.freecodecamp.com/" + leader.username}><img src={leader.img}/>{leader.username}</a>
-                </td>
-                <td>{leader.recent}</td>
-                <td>{leader.alltime}</td>
-            </tr>
-        );
-    });
-
+  // for each camper, display row of camper statistics
+  const leaderTable = props.leaders.map((leader, index) => {
     return (
-        <div>
-            <table className="table table-striped table-bordered">
-                <caption>
-                    <h1>Camper LeaderBoard</h1>
-                    <div>
-                        <h4>Sort By Top Campers:</h4>
-                        <button
-							onClick={() => props.onButtonClick("https://fcctop100.herokuapp.com/api/fccusers/top/recent")}
-						>
-							In Last 30 Days
-						</button>
-                        <button
-							onClick={() => props.onButtonClick("https://fcctop100.herokuapp.com/api/fccusers/top/alltime")}
-						>
-							Of All Time
-						</button>
-                    </div>
-                </caption>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Camper name</th>
-                        <th>Points in Last 30 Days</th>
-                        <th>Total Points</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {leaderTable}
-                </tbody>
-            </table>
-        </div>
+      <tr key={leader.username}>
+        <td>{index + 1}</td>
+        <td>
+          <a href={"https://www.freecodecamp.com/" + leader.username}><img src={leader.img} alt="User Image"/>{leader.username}</a>
+        </td>
+        <td>{leader.recent}</td>
+        <td>{leader.alltime}</td>
+      </tr>
     );
+  });
+
+  return (
+    <div>
+      <table className="table table-striped table-bordered">
+        <caption>
+          <h1>Camper LeaderBoard</h1>
+          <div>
+            <h4>Sort By Top Campers:</h4>
+            <button
+              onClick={() => props.onButtonClick("https://fcctop100.herokuapp.com/api/fccusers/top/recent")}
+            >In Last 30 Days</button>
+            <button
+              onClick={() => props.onButtonClick("https://fcctop100.herokuapp.com/api/fccusers/top/alltime")}
+            >Of All Time</button>
+          </div>
+        </caption>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Camper name</th>
+            <th>Points in Last 30 Days</th>
+            <th>Total Points</th>
+          </tr>
+        </thead>
+        <tbody>
+          {leaderTable}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 /*
@@ -86,42 +81,40 @@ const LeaderTable = (props) => {
 ██   ██ ██      ██
 */
 
+// main application
 class App extends React.Component {
-	// main application
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            leaders: [],
-            url: "https://fcctop100.herokuapp.com/api/fccusers/top/recent"
-        };
-		this.getLeaders = this.getLeaders.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      leaders: [],
+      url: "https://fcctop100.herokuapp.com/api/fccusers/top/recent"
+    };
+    this.getLeaders = this.getLeaders.bind(this);
+  }
 
-    componentDidMount() {
-		// when component mounts get top 100 campers
+  // when component mounts get top 100 campers
+  componentDidMount() {
+    this.getLeaders(this.state.url);
+  }
 
-        this.getLeaders(this.state.url);
-    }
+  // function for getting top 100 campers by most recent or all time
+  getLeaders(url) {
+    $.getJSON(url, (leaders) => {
+      this.setState({ leaders });
+    });
+  }
 
-    getLeaders(url) {
-		// function for getting top 100 campers by most recent or all time
-
-        $.getJSON(url, (leaders) => {
-            this.setState({leaders});
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <Header/>
-                <LeaderTable
-					leaders={this.state.leaders}
-					onButtonClick={this.getLeaders}/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Header/>
+        <LeaderTable leaders={this.state.leaders}
+          onButtonClick={this.getLeaders}
+        />
+      </div>
+    );
+  }
 }
 
-ReactDOM.render( <App/>, document.querySelector("#App"));
+ReactDOM.render(<App/>, document.querySelector("#App"));
